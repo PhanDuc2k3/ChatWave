6858dad12d67f240d58a586225cbf256766fb963
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2026-02-25 21:24:10 +0700

    chore: update project files

---
+++ frontend/src/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+
+export default function App() {
+  return (
+  <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+  </div>
+  );
+}
